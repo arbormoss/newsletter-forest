5e03cf5f4181f89f72cb0d937dbc2b5fb0eb0706
arbor <arbormoss@woodsprite.dev>
2025-11-07 18:20:09 -0600

    minor fix: test.sh permissions and mchimp test format

--- mchimp/mchimp_test.go
+++ mchimp/mchimp_test.go
@@ -5,13 +5,13 @@ import "testing"
 func TestHeadingMaker(t *testing.T) {
 	res := headingMaker(3)
 
-	if res != "<h3>$1</h3>" {
+	if res != "\n<h3>$1</h3>\n" {
 		t.Errorf("Incorrectly make level 3 heading: %s", res)
 	}
 
 	res = headingMaker(1)
 
-	if res != "<h1>$1</h1>" {
+	if res != "\n<h1>$1</h1>\n" {
 		t.Errorf("Incorrectly make level 1 heading: %s", res)
 	}
 }
