d36267c6f8067a9d9c1aba3629a42f85b4bbb56b
arbor <arbormoss@woodsprite.dev>
2025-11-07 18:18:02 -0600

    test: mchimp target

--- mchimp/mchimp_test.go
+++ mchimp/mchimp_test.go
@@ -1,1 +1,17 @@
 package mchimp
+
+import "testing"
+
+func TestHeadingMaker(t *testing.T) {
+	res := headingMaker(3)
+
+	if res != "<h3>$1</h3>" {
+		t.Errorf("Incorrectly make level 3 heading: %s", res)
+	}
+
+	res = headingMaker(1)
+
+	if res != "<h1>$1</h1>" {
+		t.Errorf("Incorrectly make level 1 heading: %s", res)
+	}
+}
